0b27c3cd96e08b0ec0a5d1e41f2ec299523070a3
manikant8 <102610169+manikant8@users.noreply.github.com>
2024-01-06 18:48:42 +0000

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3); in node alert has different meaning
+
+console.log(3+3); console.log(3+3) // valid but not a good coading practice
+// console.log(3+3) console.log(3+3) // this error
+console.log(3+3) 
+console.log(3+3)
+
+let names = "manikant"
+let mobile = 123456;
+let isOK = true
+console.log(typeof names); //string
+console.log(typeof mobile); //number
+console.log(typeof isOK); //boolean
+// console.log(typeof symbol); //undefined
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined

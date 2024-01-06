e6e7d1c88ecf5e3fa750439b3d202735e5315672
manikant8 <102610169+manikant8@users.noreply.github.com>
2024-01-06 18:15:04 +0000

    container config done

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,27 +1,34 @@
-// let- local scope
-// var- scope leak problem
-// const - creates a constant var with local scope
+// // let- local scope
+// // var- scope leak problem
+// // const - creates a constant var with local scope
 
-// var
-if(3 > 2){
-    var a = 5;
-    console.log(a);
-}
-console.log(a);  //"var" is visible here because of scope leak
-
-
-// let
-if(3 > 2){
-    let a = 5;
-    console.log(a);
-}
-console.log(a);  //"let" is NOT visible here. It's scope is within the block only. similarly with "const"
+// // var
+// if(3 > 2){
+//     var a = 5;
+//     console.log(a);
+// }
+// console.log(a);  //"var" is visible here because of scope leak
 
 
-// const
-if(3 > 2){
-    const a = 5;
-    console.log(a);
-}
-console.log(a);  //var is visible here because of scope leak
+// // let
+// if(3 > 2){
+//     let a = 5;
+//     console.log(a);
+// }
+// console.log(a);  //"let" is NOT visible here. It's scope is within the block only. similarly with "const"
 
+
+// // const
+// if(3 > 2){
+//     const a = 5;
+//     console.log(a);
+// }
+// console.log(a);  //var is visible here because of scope leak
+
+const rollno = "23CS06013"
+let names = 'Manikant Sharma'
+var college = "IIT BBS"
+college = "IIT BBSR"
+// rollno = 1100410030 wrong assignment
+let state; //variable declared with any assignment is treated as "undefined"
+console.table([rollno, names, college, state])

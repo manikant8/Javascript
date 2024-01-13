1019a9865fc1362d51100d0414572f18130be1fb
manikant8 <102610169+manikant8@users.noreply.github.com>
2024-01-13 20:28:48 +0530

    Add files via upload

---
+++ 01_basics/string.js
@@ -0,0 +1,22 @@
+let name = "Ashish Sharma"
+let rollNo = "23CS06013"
+
+let name2 = name
+console.log("hello, my name is " +name+  " & my Roll no is " + rollNo)
+
+// string interpolation, ${} works as placeholder
+console.log(`hello, my name is ${name} & my Roll no is ${rollNo}`)
+console.log([name, name2]);
+name2 = "Manikant Sharma"
+console.log([name, name2]);
+
+
+let name3 = new String("john");
+console.log(name);
+console.log(name3);
+console.log(name.toUpperCase());
+
+console.log([typeof name, typeof name3]);
+
+
+

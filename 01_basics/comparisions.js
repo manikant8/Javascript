b091eb89dcc081e8b7de287045c6538b622c0c22
manikant8 <102610169+manikant8@users.noreply.github.com>
2024-01-13 16:28:04 +0530

    comparisions

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,18 @@
+console.log(2 == 1);
+console.log(2 != 1);
+console.log("2" == 1);
+console.log("2" > 1);
+console.log("02" == 1);
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null < 0);
+console.log(null <= 0);  //null converted to 0
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+// strict check (===) check value & datatype both
+console.log("0" == 0);
+console.log("0" === 0);

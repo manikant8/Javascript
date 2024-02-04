b6501dd4e875d459f9fc85b2e28da618ccdeb5f7
manikant8 <102610169+manikant8@users.noreply.github.com>
2024-02-04 07:27:03 +0000

    Array.of, Array.isArray(), Array.from

--- 01_basics/array_2.js
+++ 01_basics/array_2.js
@@ -15,4 +15,16 @@ let newArr2 = [...marvel_heros,...dc_heroes, "shaktimaan"]
 
 // flat() returns NEW flatted array
 let newArr3 = [1,2,3,[4,5],[[6,4,[8]]]];
-console.log(newArr3.flat(Infinity));
+// console.log(newArr3.flat(Infinity));
+
+// Array.isArray() returns true, if parameter passed is array otherwise false
+// console.log(Array.isArray("manikant"));
+
+// converting parameter to an array
+// console.log(Array.from("manikant"));
+// console.log(Array.from({fname:"manikant",lname: "sharma"}));// it will return empty array, as it cannot convert object to array directly
+
+let marks1 = 20
+let marks2 = 57
+let marks3 = 87
+console.log(Array.of(marks1, marks2, marks3));
